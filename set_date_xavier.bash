255e58f2ca2e319d2e659291c550e813b675bdf0
Miguel Fernandez-Cortizas <miferco97@gmail.com>
2024-03-14 16:41:20 +0100

    .sh changed to .bash

---
+++ set_date_xavier.bash
@@ -0,0 +1,3 @@
+#!/bin/bash
+ssh root@cvar-xavier "date -s '$(date)'"
+ssh root@cvar-xavier-1 "date -s '$(date)'"
--- set_date_xavier.sh
+++
@@ -1,2 +0,0 @@
-ssh root@cvar-xavier "date -s '$(date)'"
-ssh root@cvar-xavier-1 "date -s '$(date)'"
